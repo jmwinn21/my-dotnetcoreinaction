0311c998b7c3681a689fa6a5a133f724aacc579e
Jacob Winn <jmwinn21@gmail.com>
2019-03-24 17:46:34 -0400

    up to section 10.2

---
+++ chapter10/ACController/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+
+namespace ACController {
+    class Program {
+        static void Main (string[] args) {
+            var culture = CultureInfo.CreateSpecificCulture ("es-MX");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            var resMan = new ResourceManager ("ACController.strings", typeof (Program).GetTypeInfo ().Assembly);
+            Console.WriteLine (resMan.GetString ("ExhaustAirTemp", culture) + TempControl.ExhaustAirTemp);
+        }
+    }
+}
